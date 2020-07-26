11d36e8f7f2a258f072be9c040c1af2d4dcac511
chaitanya-suvarna <Chaitanya.svrn@gmail.com>
2020-07-26 12:05:53 +0530

    Added DbContext for SQLite DB

---
+++ SQLiteDemoWebApi/Contexts/AthleteSQLiteDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQLiteDemoWebApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLiteDemoWebApi.Contexts
+{
+    public class AthleteSQLiteDbContext : DbContext
+    {
+        public DbSet<Athlete> Athletes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuider)
+        {
+            base.OnModelCreating(modelBuider);
+        }
+    }
+}
