33db6ea81d38f8b79f9e06a1102e1648f8e1f502
chaitanya-suvarna <Chaitanya.svrn@gmail.com>
2020-07-26 11:46:31 +0530

    Added Entity Athlete

---
+++ SQLiteDemoWebApi/Entities/Athlete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLiteDemoWebApi.Entities
+{
+    public class Athlete
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sport { get; set; }
+    }
+}
