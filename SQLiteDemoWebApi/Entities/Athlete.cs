9276e06387d3f8a8914ea64b27f78a7bf56ccc9c
chaitanya-suvarna <Chaitanya.svrn@gmail.com>
2020-07-26 12:09:45 +0530

    Added Data Anotations to the Entity class

--- SQLiteDemoWebApi/Entities/Athlete.cs
+++ SQLiteDemoWebApi/Entities/Athlete.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,14 @@ namespace SQLiteDemoWebApi.Entities
 {
     public class Athlete
     {
+        [Key]
         public int id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
         public int Age { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Sport { get; set; }
     }
 }
